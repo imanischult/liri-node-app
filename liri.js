de9c3a9f5509eed73a923da179b52f51d254b536
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-05-04 21:36:52 -0400

    added concert this function

--- liri.js
+++ liri.js
@@ -3,89 +3,127 @@
 const dotenv = require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
-const spotify = require("node-spotify-api")
+const Spotify = require("node-spotify-api");
 
 // Add the code required to import the keys.js file and store it in a variable.
 const keys = require("./keys.js");
 
 // Store key value for API keys 
 
-const spotifyID = keys.SPOTIFY_ID
-const spotifySecret = keys.SPOTIFY_SECRET
-const omdbID = keys.OMDB 
+const spotify = new Spotify(keys.spotify)
+const omdbID = process.env.OMDB
 
-// ----------------------- Global variables ----------------------------- //
 
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first! 
 
-var command = process.argv[2];
-var subject = process.argv.slice(3).join(" ");;
+const command = process.argv[2];
+const subject = process.argv.slice(3).join(" ");;
 
 
 // ------------------------ LIRI Logic -------------------------- //
 
 // The switch-case statement will direct which function gets run.
-switch (action) {
+const liriapp = function(command, data) {
+switch (command) {
     case "movie-this":
-      movieThis();
+      movieThis(data);
       break;
     
     case "spotify-this":
-      spotifyThis();
+      spotifyThis(data);
       break;
     
     case "concert-this":
-      concertThis();
+      concertThis(data);
       break;
     
     case "do-what-it-says":
       doWhatItSays();
       break;
     }
+}
 
-
-// ------------------------  MOVIE-THIS COMMAND ------------------------- //
+// ------------------------  MOVIE-THIS FUNCTION ------------------------- //
 
 // Then run a request with axios to the OMDB API with the movie specified
-var movieThis = function() {
-
-    if (command === 'movie-this') {
+const movieThis = function(movieData) {
+    
     axios
-        .get(`http://www.omdbapi.com/?t=${subject}=&plot=short&apikey=trilogy`)
+        .get(`http://www.omdbapi.com/?t=${movieData}=&plot=short&apikey=${omdbID}`)
         .then(
             function(response) {
 
-                var title = response.data.Title;
-                var year = response.data.Year;
-                var imdbRating = response.data.imdbRating;
-                var rottenTomRating = response.data.Ratings[1].Value;
-                var countryProduced = response.data.Country;
-                var language = response.data.Language;
-                var plot = response.data.Plot;
-                var actors = response.data.Actors;
+                const title = response.data.Title;
+                const year = response.data.Year;
+                const imdbRating = response.data.imdbRating;
+                const rottenTomRating = response.data.Ratings[1].Value;
+                const countryProduced = response.data.Country;
+                const language = response.data.Language;
+                const plot = response.data.Plot;
+                const actors = response.data.Actors;
 
-                console.log(`*******************\n\nTitle: ${title} \nYear: ${year} \nIMDB Rating: ${imdbRating} \nRotten Tomato Rating: ${rottenTomRating} \nCountry Where Movie Was Produced: ${countryProduced} \nMovie Language: ${language} \nPlot: ${plot} \nMain Actors: ${actors} \n\n*******************\n`);
+                console.log(`========================*\n\nTitle: ${title} \nYear: ${year} \nIMDB Rating: ${imdbRating} \nRotten Tomato Rating: ${rottenTomRating} \nCountry Where Movie Was Produced: ${countryProduced} \nMovie Language: ${language} \nPlot: ${plot} \nMain Actors: ${actors} \n\n========================*\n`);
 
-            }
-        )
-    }
+        }
+    )
 };
 
-movieThis();
+
+// ------------------------  SPOTIFY-THIS FUNCTION ------------------------- //
+
+const spotifyThis = function(concertData) {
+    axios
+        .get(`https://rest.bandsintown.com/artists/${concertData}/events?app_id=codingbootcamp`)
+        .then(
+            console.log(response)
+        )
+}
+// ------------------------  CONCERT-THIS FUNCTION ------------------------- //
+
+const concertThis = function(artistData) {
+    axios
+        .get(`https://rest.bandsintown.com/artists/${artistData}/events?app_id=codingbootcamp`)
+        .then(
+            function(response) {
+
+                const artistResponse = response.data
+
+                for (let i = 0; i < artistResponse.length; i++) {
+                    let info = artistResponse[i];
+                    
+                const venue = info.venue.name
+                const location = `${info.venue.city}, ${info.venue.region}`
+                const eventDate = moment(info.datetime).format('L')
+
+                console.log(`${venue} \n${location} \n${eventDate}`)
+            }
+                
+        }
+    )
+}
+// ------------------------  DO-WHAT-IT-SAYS FUNCTION ------------------------- //
+
+const doWhatItSays = function(concertData) {
+    axios
+        .get(`https://rest.bandsintown.com/artists/${concertData}/events?app_id=codingbootcamp`)
+        .then(
+            console.log(response)
+        )
+}
+
+// ------------------------ APP LOGIC ------------------------- //
+
+const execute = function (action, subString) {
+    liriapp(action, subString);
+}
+
+
+// ------------------------ Execute game function ------------------------- //
+
+execute(command, subject);
+
 
 // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-
-
-
-// **** Just checking that the above global variables are capturing the data. It works! **** //
-// console.log(spotifyID);
-// console.log(spotifySecret);
-
-
-// Bands in Town: "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp" //
-// OMDB: http://www.omdbapi.com/?apikey=[yourkey]&t=" + movieTitle +"
-// Spotify: 
 
 
 
