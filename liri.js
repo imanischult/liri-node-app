5cd889be112e969d093bb93746460ed938ac0041
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-05-12 15:43:33 -0400

    cleaned up some lines. added comments

--- liri.js
+++ liri.js
@@ -1,33 +1,33 @@
 
 // // Add code to read and set any environment variables with the necessary dependency packages
-const dotenv = require("dotenv").config();
-const axios = require("axios");
-const moment = require("moment");
-const Spotify = require("node-spotify-api");
-const fs = require('fs');
+const dotenv = require("dotenv").config();      // This allows us to use the .env file for required spotify information/
+const axios = require("axios");                 // This allows us to use axios to make calls to an API
+const moment = require("moment");               // This allows us to change the date formatting (moment has other features as well)
+const Spotify = require("node-spotify-api");    // This allows us to use the node spotify api
+const fs = require('fs');                       // This allows us to read from/write to other files
 
 // Add the code required to import the keys.js file and store it in a variable.
 const keys = require("./keys.js");
 
 // Store key value for API keys 
-
-const spotify = new Spotify(keys.spotify)
+const spotify = new Spotify(keys.spotify) // This pulls from the "keys.js" file where there is an object called Spotify. 
 const omdbID = process.env.OMDB
-
 
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first! 
 
-const command = process.argv[2];
-const subject = process.argv.slice(3).join(" ");;
+// We'll need this information to execute the switch case (below). The command to execute is on line 166
+const command = process.argv[2]; // Pulls the command that the user types in terminal
+const subject = process.argv.slice(3).join(" "); // Pulls anything after the command and joins them
 
 
 // ------------------------ LIRI Logic -------------------------- //
 
-// The switch-case statement will direct which function gets run.
-const liriapp = function(command, data) {
-switch (command) {
-    case "movie-this":
-      movieThis(data);
+// The switch-case statement will direct which function is ran based on the command
+const liriapp = function(theCommand, data) { // This function takes in 2 parameters: a command and data. The command comes from the user's command, and data comes from what the user enters after their command //
+
+switch (theCommand) {  // The switch happens depending on the command given
+    case "movie-this":  // If the user writes "move-this", 
+      movieThis(data); // Run the movieThis function, and so on for the below
       break;
     
     case "spotify-this":
@@ -135,7 +135,6 @@ const doWhatItSays = function(command) {
             
         // It's helpful to console.log(spotArr) to make sure it reads the random text
 
-        let commandRand = spotArr[0];
         let song = spotArr[1];
 
         spotify.search({ type: 'track', query: song}, function(err, commandResp) {
@@ -145,7 +144,7 @@ const doWhatItSays = function(command) {
            
             const songRand = commandResp.tracks.items
             
-            // The response returns an object within an object, so we have to use an object contructor to grab the info we want
+            // The response returns an object within an object, so we have to use an object contructor (for loop) to grab the info we want
     
             for (let i = 0; i < songRand.length; i++) {
                     let song = songRand[i];
@@ -154,23 +153,16 @@ const doWhatItSays = function(command) {
                     let songName = song.name;
                     let spotifyLink = song.external_urls.spotify;
                     let albumName = song.album.name;
-    
+
                     console.log(`========================\n\nArtist(s): ${artistName} \nName of Song: ${songName} \nSpotify Link: ${spotifyLink} \nAlbum Name: ${albumName}\n\n========================\n`);
                 }
             })
         })
     }
 
-// ------------------------ APP LOGIC ------------------------- //
-
-const execute = function (action, subString) {
-    liriapp(action, subString);
-}
-
-
 // ------------------------ Execute game function ------------------------- //
 
-execute(command, subject);
+liriapp(command, subject);
 
 
 
